70e62ed1e4ce47c1cb3985865f040a74e0d03a48
Vivek <viveksingh.27@yahoo.com>
2024-08-13 17:44:12 +0530

    Updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-export const server = "http://localhost:3000";
+export const server = "https://nextgenacademy.onrender.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
