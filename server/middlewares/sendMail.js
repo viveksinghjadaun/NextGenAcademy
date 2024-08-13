2a398abcfa27fc7b2b3a21265a4b1decd3b1d07c
Vivek <viveksingh.27@yahoo.com>
2024-08-13 18:18:05 +0530

    updated sendMail.js

--- server/middlewares/sendMail.js
+++ server/middlewares/sendMail.js
@@ -134,7 +134,7 @@ export const sendForgotMail = async (subject, data) => {
     <p>If you did not request this, please ignore this email.</p>
     <div class="footer">
       <p>Thank you,<br>Your Website Team</p>
-      <p><a href="https://yourwebsite.com">yourwebsite.com</a></p>
+      <p><a href="https://nextgen-academy-wheat.vercel.app/">yourwebsite.com</a></p>
     </div>
   </div>
 </body>
